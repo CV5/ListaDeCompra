7f9a1054619811c8fa6e3d176f793aa3cd2639bb
Cristian Veras <mrchrist85@gmail.com>
2019-07-12 15:33:50 -0400

    Lsita agregada

--- app/src/main/java/com/example/mylist/Listado.java
+++ app/src/main/java/com/example/mylist/Listado.java
@@ -3,21 +3,41 @@ package com.example.mylist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
-public class Listado extends AppCompatActivity {
-        ListView listView;
-        String[] list = {"jamon","Queso","Tomate","Cafe"};
-        ArrayAdapter<String> arrayAdapter;
+import java.util.ArrayList;
 
+public class Listado extends AppCompatActivity {
+        ListView listV;
+        EditText editTextItem;
+        ArrayList<String> arreglo;
+        ArrayAdapter<String> arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listado);
-        arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,list);
-        listView = findViewById(R.id.listado);
-        listView.setAdapter(arrayAdapter);
+
+        listV = findViewById(R.id.listXml);
+        editTextItem = findViewById(R.id.editTextItem);
+
+        arreglo = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<>
+                (
+                Listado.this,
+                android.R.layout.simple_list_item_1,
+                arreglo
+                );
+        listV.setAdapter(arrayAdapter);
+    }
+
+    public void agregar(View view) {
+        arreglo.add(editTextItem.getText().toString());
+        arrayAdapter.notifyDataSetChanged();
+
+
 
     }
 }
--- app/src/main/java/com/example/mylist/MainActivity.java
+++ app/src/main/java/com/example/mylist/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
         userEditText = findViewById(R.id.user);
         passEditText = findViewById(R.id.pass);
 
-
     }
 
 
